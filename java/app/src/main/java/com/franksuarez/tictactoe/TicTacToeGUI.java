ffc0b1809e91450c4ee1911d1a2827d9470d8a54
Frank Suarez <fsuarez2007@gmail.com>
2024-05-04 15:20:51 -0400

    * Basic button functionality * Sometimes, buttons do not work. This might be a problem with threading.

--- java/app/src/main/java/com/franksuarez/tictactoe/TicTacToeGUI.java
+++ java/app/src/main/java/com/franksuarez/tictactoe/TicTacToeGUI.java
@@ -5,6 +5,11 @@
 package com.franksuarez.tictactoe;
 
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.util.ArrayList;
 
 
 /**
@@ -22,25 +27,105 @@ import java.awt.*;
  * @author franksuarez
  */
 public class TicTacToeGUI extends Frame {
+    private java.util.List<Button> buttons;
+    
+    
+    
     public TicTacToeGUI() {
         
     }
     
     
+    /**
+     * Places <code>char</char> player in 
+     * 
+     * 
+     * 
+     * @param x horizontal position (0 <= x <= 2) with 0 at left.
+     * @param y vertical position (0 <= y <= 2) with 0 at the top.
+     * @param player 
+     */
+    public void takeTurn(int x, int y, char player) {
+        
+        
+        
+    }
     
+    public void addClosingListener() {
+        addWindowListener(new WindowAdapter() {
+            @Override
+            public void windowClosing(WindowEvent e) {
+                super.windowClosing(e); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+                System.exit(0);
+            }
+            
+        });
+    }
     
-    public void createMainWindow() {
-        setSize(300,300);
-        setTitle("Tic-Tac-Toe");
-        setLayout(null);
-        setVisible(true);
+    public void initializeButtonList() {
+        
+        if (this.buttons == null) {
+            this.buttons = new ArrayList<>();
+        } else {
+            // Buttons may be referenced by Frame so they may leak
+            this.buttons.clear();
+        }
+        
+        for(int n = 0; n < 9;n++) {
+            this.buttons.add(new Button(""));
+        }
+        
         
     }
     
     
+    public void addComponents() {
+        
+        // clear table
+        this.removeAll();
+        
+        for(int n = 0; n < 9;n++) {
+            this.add(this.buttons.get(n));
+        }
+        
+        
+    }
+    
+    
+    public void configureWindow() {
+        setSize(300,300);
+        setTitle("Tic-Tac-Toe");
+        setLayout(new GridLayout(3,3));
+    }
+    
+    
+    public void setupButtonOnclickCallback() {
+        for (int n = 0; n < 9; n++) {
+            
+            this.buttons.get(n).addMouseListener(new MouseAdapter() {
+                @Override
+                public void mouseClicked(MouseEvent e) {
+                    super.mouseClicked(e); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/OverriddenMethodBody
+                    Button b = (Button) e.getComponent();
+                    b.setLabel("hi");
+                }
+
+
+            });
+        }
+    }
+    
     
     public void start() {
-        createMainWindow();
+        addClosingListener();
+        configureWindow();
+        
+        initializeButtonList();
+        addComponents();
+        
+        setupButtonOnclickCallback();
+        
+        setVisible(true);
         
     }
     

a1130c534d4e28c130b721e073af040bda24bad6
Frank Suarez <fsuarez2007@gmail.com>
2024-05-23 20:26:07 -0400

    creating unit tests

--- python/tictactoe.py
+++ python/tictactoe.py
@@ -2,43 +2,77 @@
 
 
 # uses tkinter 3.11 from homebrew
-import tkinter
+
 from tkinter import *
 from tkinter import ttk
 
 
 class TicTacToeBoard:
+    # PROPERTIES
+    # currentPlayer
+    # cells
+    # winningMoves
     
-    
-    
-    pass
-    
-
-
-
-class TicTacToeApp:
-    
-    
+    def initializeWinningMoves(self):
+        
+        # list of tuples of winning moves
+        self.winningMoves = [
+            (0,0,0),
+        ]
+        
+    # STUB
+    def checkForWinner(self):
+        for move in self.winningMoves:
+            print(move)
+         
     
     def __init__(self):
+        self.player1 = 'X'
+        self.player2 = 'O'
+        
+        self.currentPlayer = self.player1
+        self.cells = []
+        
+        
+    def setCell(self,location,player):
+        self.cells[location] = player
+    
+    def getCell(self,location):
+        return self.cells[location]
+    
+    
+    
+    
+
+class TicTacToeApp:
+    def __init__(self):
+        self.tttBoard = TicTacToeBoard()
+        
+        
+        
         self.cells = {}
-        self.btnHeight = 2
-        self.btnWidth = 2
+        self.btnHeight = 5
+        self.btnWidth = 5
 
     def buttonAction(self,btn,x,y):
         "Called when button is pressed."
         btn['text'] = 'O'
-        print(f"Button {n}: {btn} was pressed.")
         
-    def callBackFactory(self,btn,x,y):
-        "Creates a 0-arity callback function for Button command."
-        return (lambda:self.buttonAction(btn,x,y))
+        
     
-    def createComponents(self):
-        self.rootWindow = Tk()
-        frm = Frame(self.rootWindow)
-        frm.grid()
+    
+    
+    def createTicTacToeFrame(self,parent=None):
         
+        # utility function for buttons
+        def callBackFactory(btn,x,y):
+            "Creates a 0-arity callback function for Button command."
+            return (lambda:self.buttonAction(btn,x,y))
+        
+        
+        
+        frm = Frame(parent,padx=5,pady=5)
+        frm.grid()
         
         for x in range(0,3):
             for y in range(0,3):
@@ -51,8 +85,25 @@ class TicTacToeApp:
                 b.grid(column=x,row=y)
                 
                 # define callback command separately so we can refer to this button
-                b['command'] = self.callBackFactory(b,x,y)
-            
+                b['command'] = callBackFactory(b,x,y)
+                b['text'] = f"({x},{y})"
+        return frm
+        
+        
+        
+    def createComponents(self):
+        self.rootWindow = Tk()
+        self.rootWindow.title('Tic-Tac-Toe')
+        
+        frm = Frame(self.rootWindow)
+        frm.grid()
+        
+        
+        statusLabel = Label(frm,text="hi")
+        statusLabel.grid(column=0,row=0)
+        
+        tttfrm = self.createTicTacToeFrame(frm)
+        tttfrm.grid(column=0,row=1)
         
         self.rootWindow.mainloop()
 
@@ -62,28 +113,15 @@ class TicTacToeApp:
     def start(self):
         self.createComponents()
 
+# =======================================================
 
 def main():
     tttApp = TicTacToeApp()
     tttApp.start()
 
+if (__name__ == '__main__'):
+    main()
 
-#main()
 
 
-def lambdaFactory(s):
-    return (lambda: f"String = {s}")
 
-def testLambda():
-    s1 = "Hello"
-    c1 = lambdaFactory(s1)
-    
-    
-    s1 = "Bye"
-    c2 = lambdaFactory(s1)
-    
-    
-    print(c1())
-    print(c2())
-
-main()
